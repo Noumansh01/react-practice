1a40ecfb02e113091762c8c21612d8072d20a248
Noumansh01 <139547809+Noumansh01@users.noreply.github.com>
2024-12-04 11:38:36 +0500

    make route path

--- react-exercise2/index.js
+++ react-exercise2/index.js
@@ -1,12 +1,29 @@
 import {createRoot} from "react-dom/client"
 import {createBrowserRouter, RouterProvider} from "react-router-dom"
 import App from "./App.js"
+import Home from "./Components/Home.jsx";
+import CountryDetail from "./Components/CountryDetails.jsx";
+import Contact from "./Components/Contact.jsx";
 const getRootById= document.getElementById("root");
 const root = createRoot(getRootById);
 const Root=createBrowserRouter([
 {
   path:"/",
-  element:<App/>
+  element:<App/>,
+  children:[
+    {
+      path:"/",
+      element:<Home/>
+    },
+    {
+      path: '/:country',
+      element: <CountryDetail/>,
+    },
+    {
+      path: '/contact',
+      element: <Contact />,
+    },
+  ]
 }
 ])
 
