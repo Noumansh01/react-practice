0be69ad58a52e9d6dca4bc14e0e7928a1db222ba
Noumansh01 <139547809+Noumansh01@users.noreply.github.com>
2024-12-05 19:43:35 +0500

    write code for dark mode

--- react-exercise2/Components/Home.jsx
+++ react-exercise2/Components/Home.jsx
@@ -3,16 +3,17 @@ import SelectMenu from './SelectMenu'
 import CountryList from './CountryList'
 import '../App.css'
 import { useState } from 'react'
+import { useOutletContext } from 'react-router-dom'
 
 const Home = () => {
   // using the lifting the state up technique
   const [inputVal, setinputVal]= useState("");
-  
+  const [isDark] = useOutletContext();
   
   return (
     <>
   
-      <main>
+      <main className={`${isDark? 'dark': ''}`}>
         <div className="search-filter-container">   
           <SearchBar setinput={setinputVal} />
           <SelectMenu />
